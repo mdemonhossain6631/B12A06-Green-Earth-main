14856245e10334f1a1c6e4c4f86611ee42b80a0a
Md. Emon Hossain <mdemonhossain20158@gmail.com>
2025-09-16 23:35:08 +0600

    My 4th Commit

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
 
+//  category-section
+const loadCategories = () => {
 
-    //  category-section
-    const loadCategories = () => {
-
-        fetch("https://openapi.programming-hero.com/api/categories")
-            .then((res) => res.json())
-            .then((json) => displayCategory(json.categories));
-    };
-
-
-    const displayCategory = (allCategories) => {
-
-        const categoryContainer = document.getElementById("category-container");
-
-        for (let category of allCategories) {
+    fetch("https://openapi.programming-hero.com/api/categories")
+        .then((res) => res.json())
+        .then((json) => displayCategory(json.categories));
+};
 
 
-            const categoryDiv = document.createElement("ul");
-            categoryDiv.innerHTML = ` <ul">
-          <li  onclick="loadTree(${category.id})" class="active:bg-green-500  cursor-pointer items-center rounded-xl w-[95%] h-[40px] mb-1 py-2 px-3 ">${category.category_name}</li>
+const displayCategory = (allCategories) => {
+
+    const categoryContainer = document.getElementById("category-container");
+
+    for (let category of allCategories) {
+
+
+        const categoryDiv = document.createElement("ul");
+        categoryDiv.innerHTML = ` <ul>
+          <li  onclick="loadTree(${category.id})" class=" cursor-pointer items-center rounded-xl w-[full] h-[40px] mb-1 py-2 mx-auto ">${category.category_name}</li>
         </ul>`
 
-            categoryContainer.append(categoryDiv);
-        };
+        categoryContainer.append(categoryDiv);
     };
-    loadCategories();
+};
+loadCategories();
 
 
-    // category wise plant
 
-    const loadTree = (id) => {
+// category wise plant
 
-        const url = `https://openapi.programming-hero.com/api/category/${id}`
-        fetch(url)
-            .then((res) => res.json())
-            .then((json) => displayTree(json.plants))
-    }
+const loadTree = (id) => {
 
-    const displayTree = (allPlants) => {
+    const url = `https://openapi.programming-hero.com/api/category/${id}`
+    fetch(url)
+        .then((res) => res.json())
+        .then((json) => displayTree(json.plants))
+}
+
+const displayTree = (allPlants) => {
 
 
-        const cardContainer = document.getElementById("card-container");
-        cardContainer.innerHTML = " ";
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = " ";
 
-        for (const plants of allPlants) {
+    for (const plants of allPlants) {
 
-            const plantDiv = document.createElement("div");
-            plantDiv.innerHTML = ` 
+        const plantDiv = document.createElement("div");
+        plantDiv.innerHTML = ` 
                 <div class="card bg-white rounded-lg shadow-lg ">
                 <img class="w-full h-60 rounded-t-xl object-cover " src="${plants.image}" alt="">
                 <div class=" p-4 ">
@@ -62,31 +62,31 @@
                 </div>
             </div> `
 
-            cardContainer.append(plantDiv);
-        };
-
-    }
-    loadTree();
-
-
-
-    //  card-section
-
-    const loadPlant = () => {
-        const url = `https://openapi.programming-hero.com/api/plants`;
-        fetch(url)
-            .then((res) => res.json())
-            .then((json) => plantDisplay(json.plants));
+        cardContainer.append(plantDiv);
     };
-    const plantDisplay = (allPlant) => {
 
-        const cardContainer = document.getElementById("card-container");
-        cardContainer.innerHTML = "";
+}
+loadTree();
 
-        for (let plants of allPlant) {
 
-            const plantDiv = document.createElement("div");
-            plantDiv.innerHTML = ` 
+
+//  card-section
+
+const loadPlant = () => {
+    const url = `https://openapi.programming-hero.com/api/plants`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((json) => plantDisplay(json.plants));
+};
+const plantDisplay = (allPlant) => {
+
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+
+    for (let plants of allPlant) {
+
+        const plantDiv = document.createElement("div");
+        plantDiv.innerHTML = ` 
                 <div class="card bg-white rounded-lg shadow-lg ">
                 <img class="w-full h-60 rounded-t-xl object-cover " src="${plants.image}" alt="">
                 <div class=" p-4 ">
@@ -102,26 +102,26 @@
                 </div>
             </div> `
 
-            cardContainer.append(plantDiv);
-        };
-
-    };
-    loadPlant();
-
-
-    // modal-plants details
-
-    const loadPlantDetails = async (id) => {
-        const url = `https://openapi.programming-hero.com/api/plant/${id}`
-        const res = await fetch(url)
-        const details = await res.json();
-        displayPlantDetails(details.plants);
+        cardContainer.append(plantDiv);
     };
 
-    const displayPlantDetails = (plants) => {
+};
+loadPlant();
 
-        const detailsModal = document.getElementById("details-container");
-        detailsModal.innerHTML = `
+
+// modal-plants details
+
+const loadPlantDetails = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/plant/${id}`
+    const res = await fetch(url)
+    const details = await res.json();
+    displayPlantDetails(details.plants);
+};
+
+const displayPlantDetails = (plants) => {
+
+    const detailsModal = document.getElementById("details-container");
+    detailsModal.innerHTML = `
                 <div>
                     <img class="w-full h-60 rounded-t-xl object-cover" src="${plants.image}" alt="">
                     <div class=" p-4 ">
@@ -136,13 +136,13 @@
     
     `;
 
-        document.getElementById("details_modal").showModal();
+    document.getElementById("details_modal").showModal();
 
-    };
+};
 
-    // Add to cart
+// Add to cart
 
-    document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
     const cartList = document.getElementById('cartList');
     const totalPrice = document.getElementById('total-price');
     let total = 0;
@@ -187,5 +187,12 @@
     });
 
 
+    const categories = document.getElementById("category-container");
+    categories.onclick = (activeBg) => {
+        if (activeBg.target.tagName === "LI") {
+            categories.querySelectorAll("li").forEach(li => li.classList.remove("bg-green-700","hover:bg-green-500", "text-white"));
+            activeBg.target.classList.add("bg-green-700", "hover:bg-green-500", "text-white");
+        }
+    }
 
 });
